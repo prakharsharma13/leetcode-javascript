01270895fccff8d8890e4d802dc8e9394f06e165
prakharsharma13 <115884239+prakharsharma13@users.noreply.github.com>
2024-07-08 23:38:36 +0530

    Hello World

---
+++ 2667-create-hello-world-function.js
@@ -0,0 +1,11 @@
+//Write a function createHelloWorld. It should return a new function that always returns "Hello World".
+
+var createHelloWorld = function(){
+    return function(...args){
+        return "Hello World"
+    }
+}
+
+
+const f = createHelloWorld();
+console.log(f());
